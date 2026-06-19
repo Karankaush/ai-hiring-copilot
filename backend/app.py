348c48925d06576d9c480c4a9a77d9549e88e621
karan <learnwithfacts05@gmail.com>
2026-06-19 08:40:44 +0530

    setup PDF extraction service

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
+from services.pdf_loader import extract_pdf_text
 
-app = FastAPI()
+app = FastAPI(
+    title="AI Hiring Copilot",
+    version="1.0.0"
+)
 
 @app.get('/')
 def home():
     return{
         'message' : 'welcome to home page. AI Hiring Copilot Backend Running'
+    }
+
+@app.get('/health')
+def health():
+    return{
+        'status' : 'healthy'
+    }
+
+@app.post('/upload-pdf')
+async def upload_pdf(file : UploadFile = File(...)):
+    file_path = f"uploads/{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+
+    extracted_text = extract_pdf_text(file_path)
+    return{
+        "filename": file.filename,
+        "characters": len(extracted_text),
+        "preview": extracted_text[:1000]
+
     }
---
+++ backend/services/pdf_loader.py
@@ -0,0 +1,14 @@
+import fitz  # PyMuPDF
+
+def extract_pdf_text(pdf_path : str) ->str:
+
+    document = fitz.open(pdf_path)
+
+    text = ""
+
+    for page in document:
+        text += page.get_text()
+
+    document.close()
+
+    return text.strip()
